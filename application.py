c7f893aa42d53944ec5a794d12509ae6de26c950
Feroz Khan <48884151+ferozk0333@users.noreply.github.com>
2024-07-07 15:28:44 +0530

    Update application.py

--- application.py
+++ application.py
@@ -48,6 +48,7 @@ def predict_datapoint():                                             #fetching d
     
     #testing app
     if __name__=="__main__":
+        
         app.run(host='0.0.0.0', port = 8080)  
     
     
